be13c61f092c2aa576ce060c85a42c79b990d41e
vadymchan <vadymchan@gmail.com>
2024-04-26 02:10:12 +0300

    add repositories

---
+++ Task2/apz-pzpi-21-2-chan-vadym-task2/EcoMeChan_Backend/EcoMeChan_Backend/Repositories/ConsumptionNormRepository.cs
@@ -0,0 +1,41 @@
+﻿// ConsumptionNormRepository.cs
+using EcoMeChan.Database;
+using EcoMeChan.Enums;
+using EcoMeChan.Models;
+using EcoMeChan.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EcoMeChan.Repositories
+{
+    public class ConsumptionNormRepository : IConsumptionNormRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ConsumptionNormRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<ConsumptionNorm> GetByUserIdAsync(int userId, ResourceType resourceType)
+        {
+            return await _context.ConsumptionNorms
+                .FirstOrDefaultAsync(cn => cn.UserId == userId && cn.ResourceType == resourceType);
+        }
+
+        public async Task<ConsumptionNorm> CreateAsync(ConsumptionNorm consumptionNorm)
+        {
+            _context.ConsumptionNorms.Add(consumptionNorm);
+            await _context.SaveChangesAsync();
+            return consumptionNorm;
+        }
+
+        public async Task<ConsumptionNorm> UpdateAsync(ConsumptionNorm consumptionNorm)
+        {
+            _context.ConsumptionNorms.Update(consumptionNorm);
+            await _context.SaveChangesAsync();
+            return consumptionNorm;
+        }
+    }
+}
