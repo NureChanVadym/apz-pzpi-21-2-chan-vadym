cee994b45622484c4d554cd37026a63e4c892b8f
vadymchan <vadymchan@gmail.com>
2024-04-26 02:12:00 +0300

    add template method pattern

---
+++ Task2/apz-pzpi-21-2-chan-vadym-task2/EcoMeChan_Backend/EcoMeChan_Backend/Services/ConsumptionNorms/IConsumptionNorms.cs
@@ -0,0 +1,17 @@
+﻿// IConsumptionNormService.cs
+
+
+using EcoMeChan.Enums;
+using System;
+using System.Threading.Tasks;
+
+namespace EcoMeChan_Backend.Services.ConsumptionNorms
+{
+    
+    public interface IConsumptionNormService
+    {
+        // template method
+        Task CalculateConsumptionNormAsync(int userId, ResourceType resourceType, DateTime startDate, DateTime endDate);
+        Task<bool> DetectAnomaliesAsync(int userId, ResourceType resourceType, decimal currentConsumption);
+    }
+}
