325af57b23bb2ceec954483370e80641414502db
vadymchan <vadymchan@gmail.com>
2024-04-26 02:12:21 +0300

    add controllers

---
+++ Task2/apz-pzpi-21-2-chan-vadym-task2/EcoMeChan_Backend/EcoMeChan_Backend/Controllers/ConsumptionNormController.cs
@@ -0,0 +1,36 @@
+﻿// ConsumptionNormController.cs
+
+using EcoMeChan.Enums;
+using EcoMeChan_Backend.Services.ConsumptionNorms;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+
+namespace EcoMeChan.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ConsumptionNormController : ControllerBase
+    {
+        private readonly IConsumptionNormService _consumptionNormService;
+
+        public ConsumptionNormController(IConsumptionNormService consumptionNormService)
+        {
+            _consumptionNormService = consumptionNormService;
+        }
+
+        [HttpPost("calculate")]
+        public async Task<IActionResult> CalculateConsumptionNorm(int userId, ResourceType resourceType, DateTime startDate, DateTime endDate)
+        {
+            await _consumptionNormService.CalculateConsumptionNormAsync(userId, resourceType, startDate, endDate);
+            return Ok();
+        }
+
+        [HttpGet("detect-anomalies")]
+        public async Task<IActionResult> DetectAnomalies(int userId, ResourceType resourceType, decimal currentConsumption)
+        {
+            var isAnomaly = await _consumptionNormService.DetectAnomaliesAsync(userId, resourceType, currentConsumption);
+            return Ok(new { IsAnomaly = isAnomaly });
+        }
+    }
+}
