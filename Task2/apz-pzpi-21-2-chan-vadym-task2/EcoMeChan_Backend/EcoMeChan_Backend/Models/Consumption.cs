8834060612fa8d3507694c04ed1b97b55987422d
vadymchan <vadymchan@gmail.com>
2024-04-26 02:07:53 +0300

    add models

---
+++ Task2/apz-pzpi-21-2-chan-vadym-task2/EcoMeChan_Backend/EcoMeChan_Backend/Models/Consumption.cs
@@ -0,0 +1,21 @@
+﻿// Consumption.cs
+
+
+using EcoMeChan.Enums;
+
+namespace EcoMeChan.Models
+{
+    public class Consumption
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int TariffId { get; set; }
+        public DateTime Date { get; set; }
+        public ResourceType ResourceType { get; set; }
+        public decimal ConsumedAmount { get; set; }
+        public decimal ConsumedCost { get; set; }
+
+        public User User { get; set; }
+        public Tariff Tariff { get; set; }
+    }
+}
---
+++ Task2/apz-pzpi-21-2-chan-vadym-task2/EcoMeChan_Backend/EcoMeChan_Backend/Models/Tariff.cs
@@ -0,0 +1,18 @@
+﻿// Tariff.cs
+
+using EcoMeChan.Enums;
+
+namespace EcoMeChan.Models
+{
+    public class Tariff
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public ResourceType ResourceType { get; set; }
+        public decimal PricePerUnit { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+
+        public ICollection<Consumption> Consumptions { get; set; }
+    }
+}
