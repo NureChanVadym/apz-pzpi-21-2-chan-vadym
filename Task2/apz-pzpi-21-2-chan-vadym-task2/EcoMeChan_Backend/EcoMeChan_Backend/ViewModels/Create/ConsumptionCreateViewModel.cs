023206c57f200bb757d3ff5074cdc9909ef26dd8
vadymchan <vadymchan@gmail.com>
2024-04-26 02:08:25 +0300

    add CreateViewModel

---
+++ Task2/apz-pzpi-21-2-chan-vadym-task2/EcoMeChan_Backend/EcoMeChan_Backend/ViewModels/Create/ConsumptionCreateViewModel.cs
@@ -0,0 +1,15 @@
+﻿// ConsumptionCreateViewModel.cs
+using EcoMeChan.Enums;
+using System;
+
+namespace EcoMeChan.ViewModels.Create
+{
+    public class ConsumptionCreateViewModel
+    {
+        public int UserId { get; set; }
+        public int TariffId { get; set; }
+        public DateTime Date { get; set; }
+        public ResourceType ResourceType { get; set; }
+        public decimal ConsumedAmount { get; set; }
+    }
+}
---
+++ Task2/apz-pzpi-21-2-chan-vadym-task2/EcoMeChan_Backend/EcoMeChan_Backend/ViewModels/Create/TariffCreateViewModel.cs
@@ -0,0 +1,15 @@
+﻿// TariffCreateViewModel.cs
+using EcoMeChan.Enums;
+using System;
+
+namespace EcoMeChan.ViewModels.Create
+{
+    public class TariffCreateViewModel
+    {
+        public string Name { get; set; }
+        public ResourceType ResourceType { get; set; }
+        public decimal PricePerUnit { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+    }
+}
